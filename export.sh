a1c768ca2aa429e4da79637cee567b5f3eb16ab7
vagrant <vagrant@homestead>
2015-12-18 07:37:31 +0000

    andrew code

--- export.sh
+++ export.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# get
-for i in $(identify -format "%[label] %[scene]\n" test.psd); do
-    # do convert test.psd[0] test.psd[$i] \( -clone 0 -alpha transparent \) -swap 0 +delete -coalesce -compose src-over -composite $i.png
-    echo item: $i
+infile=test.psd
+prefix='test-'
+
+# convert test.psd[0] test.psd[$i] \( -clone 0 -alpha transparent \) -swap 0 +delete -coalesce -compose src-over -composite $i.png
+
+i=1
+for layername in $(convert $infile -verbose info: | grep "label:" | cut -d: -f 2 | cut -d' ' -f2)
+    do
+    	convert $infile[0] $infile[$i] \( -clone 0 -alpha transparent \) -swap 0 +delete -coalesce -compose src-over -composite $prefix$layername.png
+		echo $layername
+		let i++
 done
