99131ce1b41872307a9ad2234f76b2aa2e60174c
vagrant <vagrant@homestead>
2015-12-18 07:39:10 +0000

    cleanup

--- export.sh
+++ export.sh
@@ -2,10 +2,8 @@
 
 infile=test.psd
 prefix='test-'
-
-# convert test.psd[0] test.psd[$i] \( -clone 0 -alpha transparent \) -swap 0 +delete -coalesce -compose src-over -composite $i.png
-
 i=1
+
 for layername in $(convert $infile -verbose info: | grep "label:" | cut -d: -f 2 | cut -d' ' -f2)
     do
     	convert $infile[0] $infile[$i] \( -clone 0 -alpha transparent \) -swap 0 +delete -coalesce -compose src-over -composite $prefix$layername.png
